d51c734e6ee71c2d133e5837df048f834ff39d4e
kramoris <kramoris@proton.me>
2026-04-04 16:27:23 +0200

    add --verbose flag to show more info

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ def main():
 
     parser = argparse.ArgumentParser(description="AI Agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
     client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(model=model, contents=messages)
     if not response.usage_metadata:
         raise RuntimeError("No usage metadata found")
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
+    if args.verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
     print(f"Response:\n{response.text}")
 
 
