736427c172e79b21ca4816f73089d1efceee0625
kramoris <kramoris@proton.me>
2026-04-04 16:11:44 +0200

    add message history support to gemini api call

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
 
     model = "gemini-2.5-flash"
-
-    parser = argparse.ArgumentParser(description="AI Agent")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("API key not found")
 
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key=api_key)
-    generated_content = client.models.generate_content(model=model, contents=args.user_prompt)
-    if not generated_content.usage_metadata:
+    response = client.models.generate_content(model=model, contents=messages)
+    if not response.usage_metadata:
         raise RuntimeError("No usage metadata found")
 
-    prompt_tokens = generated_content.usage_metadata.prompt_token_count
-    response_tokens = generated_content.usage_metadata.candidates_token_count
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
     print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
-    print(f"Response:\n{generated_content.text}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
